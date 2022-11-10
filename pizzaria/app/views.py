3e1050b1b784d498def613e01a20723e51848837
MatthewLC <mateusluiztj@hotmail.com>
2022-11-10 08:11:44 -0300

    setup [1] Adding routing files

--- pizzaria/app/views.py
+++ pizzaria/app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def main(request):
+    return HttpResponse("<body><h1><center>Page in construction</center></h1></body>")
