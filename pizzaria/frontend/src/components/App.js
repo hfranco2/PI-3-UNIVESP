f9c73e937fb5d8b79c3e2c16f561a6ea0de6d345
hfranco2 <francohenrique10@gmail.com>
2022-11-13 10:14:34 -0300

    minidrawer default

--- pizzaria/frontend/src/components/App.js
+++ pizzaria/frontend/src/components/App.js
@@ -1,13 +1,28 @@
-import React, {Component} from "react";
-import {render} from "react-dom";
+import React, { Component } from "react";
+import ReactDOM from 'react-dom/client';
 
+import MiniDrawer from "./MiniDrawer";
 export default class App extends Component {
-    constructor(props){
-        super(props)
-    }
-    render(){
-        return (<h1>testando</h1>)
-    }
+	constructor(props) {
+		super(props);
+  
+	}
+	render() {
+
+		return (
+			<div>
+				<MiniDrawer/>
+				<h1>testando</h1>
+               
+
+			</div>
+		);
+	}
 }
-const appDiv =document.getElementById("app");
-render(<App/>, appDiv)
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
