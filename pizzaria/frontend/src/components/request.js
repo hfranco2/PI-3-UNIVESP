5fac9f56b3f0ae899104768e7db2aea7c69a238d
hfranco2 <francohenrique10@gmail.com>
2022-11-13 22:04:01 -0300

    add input phone and mask

--- pizzaria/frontend/src/components/request.js
+++ pizzaria/frontend/src/components/request.js
@@ -1,7 +1,20 @@
 import * as React from "react";
 import { styled, alpha, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import InputMask from "react-input-mask";
+import { width } from "@mui/system";
+import PropTypes from "prop-types";
+import { IMaskInput } from "react-imask";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+//Custom Components
 import MiniDrawer from "./MiniDrawer";
 const DrawerHeader = styled("div")(({ theme }) => ({
 	display: "flex",
@@ -12,45 +25,91 @@ const DrawerHeader = styled("div")(({ theme }) => ({
 	// necessary for content to be below app bar
 	...theme.mixins.toolbar,
 }));
+const TextMaskCustom = React.forwardRef(function TextMaskCustom(props, ref) {
+	const { onChange, ...other } = props;
+	return (
+		<IMaskInput
+			{...other}
+			mask="(00) 00000-0000"
+			definitions={{
+				"#": /[1-9]/,
+			}}
+			inputRef={ref}
+			onAccept={(value) =>
+				onChange({ target: { name: props.name, value } })
+			}
+			overwrite
+		/>
+	);
+});
+
+TextMaskCustom.propTypes = {
+	name: PropTypes.string.isRequired,
+	onChange: PropTypes.func.isRequired,
+};
 export default function RequestPage() {
+	const [values, setValues] = React.useState({
+		textmask: "(XX) XXXXX-XXXX",
+		numberformat: "1320",
+	});
+	const handleChange = (event) => {
+		setValues({
+			...values,
+			[event.target.name]: event.target.value,
+		});
+	};
 	return (
 		<Box sx={{ display: "flex" }}>
 			<MiniDrawer />
 			<Box component="main" sx={{ flexGrow: 1, p: 3 }}>
 				<DrawerHeader />
-				<Typography paragraph>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-					do eiusmod tempor incididunt ut labore et dolore magna
-					aliqua. Rhoncus dolor purus non enim praesent elementum
-					facilisis leo vel. Risus at ultrices mi tempus imperdiet.
-					Semper risus in hendrerit gravida rutrum quisque non tellus.
-					Convallis convallis tellus id interdum velit laoreet id
-					donec ultrices. Odio morbi quis commodo odio aenean sed
-					adipiscing. Amet nisl suscipit adipiscing bibendum est
-					ultricies integer quis. Cursus euismod quis viverra nibh
-					cras. Metus vulputate eu scelerisque felis imperdiet proin
-					fermentum leo. Mauris commodo quis imperdiet massa
-					tincidunt. Cras tincidunt lobortis feugiat vivamus at augue.
-					At augue eget arcu dictum varius duis at consectetur lorem.
-					Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-					sapien faucibus et molestie ac.
-				</Typography>
-				<Typography paragraph>
-					Consequat mauris nunc congue nisi vitae suscipit. Fringilla
-					est ullamcorper eget nulla facilisi etiam dignissim diam.
-					Pulvinar elementum integer enim neque volutpat ac tincidunt.
-					Ornare suspendisse sed nisi lacus sed viverra tellus. Purus
-					sit amet volutpat consequat mauris. Elementu sm eu facilisis
-					sed odio morbi. Euismod lacinia at quis risus sed vulputate
-					odio. Morbi tincidunt ornare massa eget egestas purus
-					viverra accumsan in. In hendrerit gravida rutrum quisque non
-					tellus orci ac. Pellentesque nec nam aliquam sem et tortor.
-					Habitant morbi tristique senectus et. Adipiscing elit duis
-					tristique sollicitudin nibh sit. Ornare aenean euismod
-					elementum nisi quis eleifend. Commodo viverra maecenas
-					accumsan lacus vel facilisis. Nulla posuere sollicitudin
-					aliquam ultrices sagittis orci a.
-				</Typography>
+				<Grid container spacing="1">
+					<Box
+						sx={{
+							width: 1 / 3,
+							height: 100,
+						}}
+					>
+						<Input
+							value={values.textmask}
+							onChange={handleChange}
+							name="textmask"
+							id="formatted-text-mask-input"
+							inputComponent={TextMaskCustom}
+					label="teste"
+							variant="outlined"
+							md={12}
+							sx={{
+								width: 1 / 1,
+								minWidth: 210,
+							}}
+						/>
+						<ButtonGroup
+							variant="contained"
+							aria-label="outlined primary button group"
+							md={12}
+							sx={{
+								width: 1 / 1,
+								minWidth: 210,
+							}}
+						>
+							<Button
+								sx={{
+									width: 1 / 2,
+								}}
+							>
+								Buscar Pedido
+							</Button>
+							<Button
+								sx={{
+									width: 1 / 2,
+								}}
+							>
+								Realizar Pedido
+							</Button>
+						</ButtonGroup>
+					</Box>
+				</Grid>
 			</Box>
 		</Box>
 	);
